97f0d439c096e68b4f4c51d3d6a1d7ea0f288657
CC-Vltava <1621737960@qq.com>
2022-07-02 16:23:20 +0800

    完成了合法位置的检查

--- main.py
+++ main.py
@@ -7,13 +7,47 @@ COLOR_WHITE = 1
 COLOR_NONE = 0
 random.seed(0)
 
+posx = [-1, -1, -1, 0, 0, 0, 1, 1, 1]
+posy = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+
+range_x = 7
+range_y = 7
+
+
+def check_position_illegal(x, y):
+    return 0 <= x <= range_x and 0 <= y <= range_y
+
+
+def check_direction(chessboard, x, y, direction, b, w, sta):
+    if not check_position_illegal(x, y):
+        return False
+    if chessboard[x][y] == 0:
+        return 0
+    if chessboard[x][y] == 1:
+        w = w + 1
+    else:
+        b = b + 1
+    if b + w == 1:
+        sta = chessboard[x][y]
+    if sta == 1 and w == 2:
+        return b > 0
+    if sta == -1 and b == 2:
+        return w > 0
+    return check_direction(chessboard, x + posx[direction], y + posx[direction], direction, b, w, sta)
+
+
+def check_pos(chessboard, x, y):
+    for i in range(8):
+        if check_direction(chessboard, x, y, i, 0, 0, 0):
+            return True
+    return False
+
 
 # don't change the class name
 class AI(object):
 
     # chessboard_size, color, time_out passed from agent
     def __init__(self, chessboard_size, color, time_out):
-
         self.chessboard_size = chessboard_size
         # You are white or black
         self.color = color
@@ -22,16 +56,15 @@ class AI(object):
         # You need add your decision into your candidate_list. System will get the end of your candidate_list as your decision .
         self.candidate_list = []
 
-
     # The input is current chessboard.
     def go(self, chessboard):
-
         # Clear candidate_list, must do this step
         self.candidate_list.clear()
         # ==================================================================
         # Write your algorithm here
         # Here is the simplest sample:Random decision
         idx = np.where(chessboard == COLOR_NONE)
+
         idx = list(zip(idx[0], idx[1]))
         # ==============Find new pos========================================
         # Make sure that the position of your decision in chess board is empty.
@@ -39,6 +72,8 @@ class AI(object):
         # Add your decision into candidate_list, Records the chess board
         # You need add all the positions which is valid
         # candidate_list example: [(3,3),(4,4)]
+
+
 
         # You need append your decision at the end of the candidate_list,
         # we will pickthe last element of the candidate_list as the position you choose
